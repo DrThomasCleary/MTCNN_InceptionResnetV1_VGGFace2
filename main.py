7092e5204a9f4ab9c849c5fc980a5ee5562a9335
DrThomasCleary <118690399+DrThomasCleary@users.noreply.github.com>
2023-03-10 15:27:34 +0000

    1 method of face rec roughly 80% accuracy
    here is am taking 2 random pictures from the files and taking their embeddings, comparing them and then checking if it has recognised the right person or not.

--- main.py
+++ main.py
@@ -5,79 +5,72 @@ from torchvision import datasets
 from torch.utils.data import DataLoader
 import os
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+import random
 
 # initializing MTCNN and InceptionResnetV1 
-mtcnn = MTCNN(image_size=100, margin=24, keep_all=False, min_face_size=100)
+mtcnn = MTCNN(image_size=100, margin=24, keep_all=False, min_face_size=50)
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
 
-# load the training dataset (insert training2ormore for better performance)
-train_dataset = datasets.ImageFolder('/Users/br/Software/Machine_learning/MTCNN-VGGFace2-InceptionResnetV1/LFW_dataset/training2ormore')
-train_loader = DataLoader(train_dataset, collate_fn=lambda x: x[0])
+# load the dataset
+dataset = datasets.ImageFolder('/Users/br/Software/Machine_learning/MTCNN-VGGFace2-InceptionResnetV1/lfw')
+dataset.idx_to_class = {i: c for c, i in dataset.class_to_idx.items()}
+loader = DataLoader(dataset, collate_fn=lambda x: x[0], shuffle=True, batch_size=100)
 
-# load the test dataset (insert testing2ormore for better performance)
-test_dataset = datasets.ImageFolder('/Users/br/Software/Machine_learning/MTCNN-VGGFace2-InceptionResnetV1/LFW_dataset/testing2ormore')
-test_loader = DataLoader(test_dataset, collate_fn=lambda x: x[0])
-
-# generate embeddings for the training dataset
-train_name_list = []
-train_embedding_list = []
-for image, index in train_loader:
+# generate embeddings for the dataset
+embedding_list = []
+name_list = []
+for image, index in loader:
     face, face_prob = mtcnn(image, return_prob=True)
-    if face is not None and face_prob > 0.99:
+    if face is not None and face_prob > 0.90:
         emb = resnet(face.unsqueeze(0))
-        train_embedding_list.append(emb.detach())
-        train_name_list.append(train_dataset.classes[index])
+        embedding_list.append(emb.detach())
+        name_list.append(dataset.idx_to_class[index])
     else:
-        print("No face detected in image training file:", index)
+        print("No face detected in image:", index)
 
-# generate embeddings for the test dataset
-test_name_list = []
-test_embedding_list = []
-for image, index in test_loader:
-    face, face_prob = mtcnn(image, return_prob=True)
-    if face is not None and face_prob > 0.99:
-        emb = resnet(face.unsqueeze(0))
-        test_embedding_list.append(emb.detach())
-        test_name_list.append(test_dataset.classes[index])
-        
-    else:
-        print("No face detected in image testing file:", index)
-
-# calculate the minimum distance between the embeddings of the detected faces and the embeddings of the known faces
-def minimum_distance(embedding_list, emb):
-    dist_list = []
-    for emb_db in embedding_list:
-        dist = torch.dist(emb, emb_db).item()
-        dist_list.append(dist)
-
-    if len(dist_list) > 0:
-        min_dist = min(dist_list)
-        min_dist_index = dist_list.index(min_dist)
-        name = train_name_list[min_dist_index]
-        return name, min_dist, dist_list
-    else:
-        return 'Unknown', None, []
-
-# calculate the accuracy of the model
+# calculate the accuracy and F1 score of the model
+num_pairs = 50
+num_correct = 0
+dist_list = []
 y_true = []
 y_pred = []
-dist_list = []
-for i, emb in enumerate(test_embedding_list):
-    name_true = test_name_list[i]
-    name_pred, min_dist, distances = minimum_distance(train_embedding_list, emb)
-    y_true.append(name_true)
-    y_pred.append(name_pred)
-    dist_list.append(min_dist)
 
-accuracy = accuracy_score(y_true, y_pred)
-precision = precision_score(y_true, y_pred, average='macro', zero_division=1)
-recall = recall_score(y_true, y_pred, average='macro', zero_division=1)
-f1 = f1_score(y_true, y_pred, average='macro', zero_division=1)
-avg_min_dist = sum(dist_list) / len(dist_list)
 
+for i in range(num_pairs*2):
+    idx1 = random.randint(0, len(embedding_list)-1)
+    idx2 = random.randint(0, len(embedding_list)-1)
+    emb1 = embedding_list[idx1] 
+    emb2 = embedding_list[idx2]
+    dist = torch.dist(emb1, emb2).item()
+    dist_list.append(dist)
+    print(idx1)
+    print(idx2)
+    print(i)
+    print(torch.equal(emb1, emb2))
+    is_match = name_list[idx1] == name_list[idx2]
+    y_true.append(is_match)
+    y_pred.append(dist < 1.0)
+    if (dist < 1.0 and is_match) or (dist > 1.0 and not is_match):
+        num_correct += 1
+
+print(y_true)
+print(y_pred)        
+accuracy = num_correct / num_pairs / 2
+precision = precision_score(y_true, y_pred)
+recall = recall_score(y_true, y_pred)
+f1 = f1_score(y_true, y_pred)
+
+# calculate ROC curve and area under the curve (AUC)
+fpr, tpr, _ = roc_curve(y_true, dist_list)
+auc_score = auc(fpr, tpr)
+
+print(dist_list)
+print(fpr)
+print(tpr)
 print("Accuracy: ", accuracy)
 print("Precision: ", precision)
 print("Recall: ", recall)
-print("F1-score: ", f1)
-print("Average minimum distance: ", avg_min_dist)
+print("F1 score: ", f1)
+print("AUC: ", auc_score)
